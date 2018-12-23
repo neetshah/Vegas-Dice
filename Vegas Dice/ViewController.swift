6d1cd636a833d1e3b8ac3d2cc1958406b7380bf2
Neet Shah <shahn17@mcmaster.ca>
2018-12-22 23:28:41 -0500

    Generate random int variables to set the dice to different values

--- Vegas Dice/ViewController.swift
+++ Vegas Dice/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Vegas Dice
+//  Vegas Dice 
 //
 //  Created by Neet Shah on 2018-12-22.
 //  Copyright © 2018 Neet Shah. All rights reserved.
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1:Int = 0
+    var randomDiceIndex2:Int = 0
+    
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+    }
 
 }
 
